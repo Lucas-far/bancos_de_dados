804d6051566f97989be14ff001b8062189133ee1
luksadmin <lukszwei2@gmail.com>
2021-01-21 20:57:58 -0300

    New files added and edited

---
+++ aulas/aula_19.py
@@ -0,0 +1,21 @@
+
+
+"""
+Módulo: aula_17.py
+Palavra chave: mysql workbench windows
+
+Curso || Bancos de Dados SQL e NoSQL do básico ao avançado
+Local || Seção 3:Modelagem de Dados
+Aula  || 19. Download, Instalação e Configuração do MySQL Workbench no Mac
+"""
+
+def parte1():
+    """
+    1 - Procurar: MySQL Workbench download
+    2 - Fazer o download do instalador
+    3 - Abrir o executável
+    4 - Mover o ícone para o diretório [ applications ] (talvez o procedimento possa ser feito no próprio instalador)
+    5 - Esperar a instalação e finalizar
+    6 - Abrir o [ Launchpad ] do MAC e abrir o aplicativo [ MySQL Workbench ]
+    7 - LEMBRETE: se o [ MySQL server ] não estiver instalado, o [ MySQL Workbench ] não carrega sua interface
+    """
